d00f802e9f8687287ce32d2779b24bf9fb3da584
LudmilaMagri <etchebestmarcos@gmail.com>
2021-06-20 10:51:03 -0300

    Ejercicio entregable 4 catalogo con maps

---
+++ src/Components/ItemList/ItemList.js
@@ -0,0 +1,37 @@
+import {React, useEffect} from 'react'
+import './ItemList.css'
+import Item from '../Item/Item'
+import { render } from '@testing-library/react'
+
+function ItemList({productos}) {
+    
+    useEffect(() => {
+        const desafioPromesa = new Promise((resolve,reject)=>{
+            resolve('Promesa cumplida!')
+            reject('Error inesperado!')
+        })
+        desafioPromesa.then(
+            setTimeout(() => {
+                productos.map((e,index)=>{
+                    return render(<Item 
+                    name={e.name} 
+                    pictureUrl={e.pictureUrl}
+                    stock={e.stock} 
+                    price={e.price} 
+                    description={e.description}  
+                    id={index} 
+                    />)
+                })
+            }, 2000)
+        )
+
+    }, [productos])
+    
+
+    return (
+        <div className="itemlist">
+        </div>
+    )
+}
+
+export default ItemList
--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,11 @@
 import {React} from "react";
 import "./ItemListContainer.css";
-import ItemCount from '../ItemCount/ItemCount.js'
+/* import ItemCount from '../ItemCount/ItemCount.js' */
+import ItemList from '../ItemList/ItemList'
 
 function ItemListContainer() {
 
-  const item = {
+/*   const item = {
     name: "BEEPLE-STRATO.9",
     url: 'https://pbs.twimg.com/media/E3-a32yXIAQKyjb.jpg',
     stock:5
@@ -18,14 +19,36 @@ function ItemListContainer() {
     name:"KIDMOGRAPH",
     url: 'https://64.media.tumblr.com/0dac9c4a0d340e5de41a37f351d6773d/094e648c7e3710ba-5a/s500x750/26ad1bda56c4afca0492e07f38ea2e470a084d9d.gifv',
     stock:25
-  }
+  } */
+
+  const falseBbdd = [
+    {
+        name: "BEEPLE-STRATO.9",
+        pictureUrl: 'https://pbs.twimg.com/media/E3-a32yXIAQKyjb.jpg',
+        description: 'Piece rendered in Octane 4',
+        price:"U$D 550.000",
+        stock:5
+    },
+    {
+        name:"MotionArtJournal",
+        pictureUrl: 'https://64.media.tumblr.com/a0e5e779b1d9af68431f2c4fe09fd0b0/88715e85981a4062-2e/s540x810/d3398cb2aa9aeb3857d3a09978984decf0b40175.gifv',
+        description: 'The aclaimed Tumblr artist tries NFT',
+        price:"U$D 57.000",
+        stock:15
+    },
+    {
+        name:"KIDMOGRAPH",
+        pictureUrl: 'https://64.media.tumblr.com/0dac9c4a0d340e5de41a37f351d6773d/094e648c7e3710ba-5a/s500x750/26ad1bda56c4afca0492e07f38ea2e470a084d9d.gifv',
+        description: 'The argentinian breaks the internet',
+        price:"U$D 150.000",
+        stock:25
+    }
+]
 
   return (
     <div className="listContainer">
       <div className="listContainer_card">
-        <ItemCount img={item.url} item={item.name} stock={item.stock} initial="0" />
-        <ItemCount img={item2.url} item={item2.name} stock={item2.stock} initial="0" />
-        <ItemCount img={item3.url} item={item3.name} stock={item3.stock} initial="0" />
+      <ItemList productos={falseBbdd} />
       </div>
     </div>
   );
