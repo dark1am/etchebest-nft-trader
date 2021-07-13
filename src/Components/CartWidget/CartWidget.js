6d43808042902abc6e21a93c4885db54beac9e4e
Marcos Etchebest <etchebestmarcos@gmail.com>
2021-07-13 10:04:32 -0300

    resuelvo entregable: 11

--- src/Components/CartWidget/CartWidget.js
+++ src/Components/CartWidget/CartWidget.js
@@ -1,13 +1,30 @@
-import {React,useContext} from "react";
+import {React,useContext,useEffect,useState} from "react";
 import "./CartWidget.css";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import {CartContext} from '../../context/cartContext'
 
 function CartWidget() {
 
+  const {listaProductos} = useContext(CartContext)
+  const [acumulador,setAcumulador] = useState(0)
+
+  useEffect(() => {
+    if(listaProductos.length>0){
+      setAcumulador(listaProductos.reduce((acum,e)=>acum+e.quantity,0))
+    } else {
+      setAcumulador(0)
+    }
+  }, [listaProductos])
+
   return (
     <div>
-      <ShoppingCartIcon style={{ color: "white", cursor: "pointer" }} />
+      {acumulador>0 && (
+      <div>
+        <h5>{acumulador}</h5>
+        <ShoppingCartIcon style={{ color: "white", cursor: "pointer" }}/>
+      </div>
+      )}
+      
     </div>
   );
 }
--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -1,10 +1,20 @@
 import {React,useContext,useEffect,useState} from 'react'
+import { Link } from 'react-router-dom'
 import Page from '../../Components/Page/Page'
 import {CartContext} from '../../context/cartContext'
 
 function Cart() {
 
     const {listaProductos,removeItem,clear} = useContext(CartContext)
+    const [acumulador,setAcumulador] = useState(0)
+
+    useEffect(() => {
+        if(listaProductos.length>0){
+          setAcumulador(listaProductos.reduce((acum,e)=>acum+Number(e.item.price),0))
+        } else {
+          setAcumulador(0)
+        }
+      }, [listaProductos])
 
     return (
         <Page>
@@ -21,7 +31,16 @@ function Cart() {
                     )
                 )
             }
+            {
+                listaProductos.length===0 && (
+                    <>
+                        <h1>No hay items en el carrito!</h1>
+                        <Link to="/">Ir al catalogo!</Link>
+                    </>
+                )
+            }
             </ul>
+            <h1>Precio final: U$D {acumulador} </h1>
             <button onClick={clear}>VACIAR EL CARRITO</button>
         </Page>
     )
