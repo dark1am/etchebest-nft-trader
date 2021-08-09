843f8635bfcc015da78d1ff056a66241d7e1ceeb
Marcos Etchebest <etchebestmarcos@gmail.com>
2021-08-09 09:23:09 -0300

    elimino codigo innecesario/inutilizado: pages, market, profile, news

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import React from "react";
 import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import Cart from './Pages/Cart/Cart'
 import NotFound from './Pages/NotFound/NotFound'
-import Market from './Pages/Market/Market'
-import News from './Pages/News/News'
-import Profile from './Pages/Profile/Profile'
 import ItemListContainer from './Pages/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './Pages/ItemDetailContainer/ItemDetailContainer'
 
@@ -15,15 +12,6 @@ function App() {
         <Switch>
           <Route exact path="/">
             <ItemListContainer />
-          </Route>
-          <Route path='/market'>
-            <Market />
-          </Route>
-          <Route path='/news'>
-            <News />
-          </Route>
-          <Route path='/profile'>
-            <Profile />
           </Route>
           <Route path='/cart'>
             <Cart />
